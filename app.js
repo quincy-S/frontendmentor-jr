91d0a2d737f3fb63baa6d182de90fa3854c714df
Quincy Smith-Assan <87392433+quincy-S@users.noreply.github.com>
2022-05-10 09:41:03 +0000

    Update app.js

--- app.js
+++ app.js
@@ -8,14 +8,14 @@ elements.forEach(element => {
     })
 })
 
-if (window.location.href == "index.html"){
+if (window.location.href == "/index.html"){
     btn.addEventListener("click", (e)=>{
         localStorage.setItem("b", x)
         successPage()
     })
 }
 
-if (window.location.href == "success.html"){
+if (window.location.href == "/success.html"){
     let y = localStorage.getItem("b")
     document.getElementById("targ").innerHTML=`You selected ${y} out of 5`
 }
